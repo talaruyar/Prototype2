f240fade501611509d2117a7968fea626f283f34
Talar Uyar <talaruyar@hotmail.com>
2022-04-05 13:21:38 +0300

    the first and the only commit of the project

---
+++ Assets/Scripts/MoveForward.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+    public float speed;
+    public float topBound = 30;
+    public float lowerBound = -10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < lowerBound)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
